596430f6b2d9d2bcea9acf775d4d6467be3e196a
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-11-30 22:20:16 -0500

    Setup pull and push to remote without merge

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,27 +1,81 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/ssh"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/config"
+	gitssh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+	"io/ioutil"
+	"log"
+	"path"
 
 	"github.com/spf13/cobra"
 )
 
+var remoteUrl string
+var keyFile string
+
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sync with a remote",
+	Long:  "Pull changes from a remote repository and submit updates",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sync called")
+		pem, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		signer, err := ssh.ParsePrivateKey(pem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		auth := &gitssh.PublicKeys{User: "git", Signer: signer}
+
+		journalPath := path.Clean(viper.Get("journal-dir").(string))
+		r, err := git.PlainOpen(journalPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = r.DeleteRemote("origin")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = r.CreateRemote(&config.RemoteConfig{
+			Name: "origin",
+			URLs: []string{remoteUrl},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w, err := r.Worktree()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = w.Pull(&git.PullOptions{
+			RemoteName: "origin",
+			Auth:       auth,
+		})
+		if err != nil && err.Error() != "remote repository is empty" {
+			log.Fatal(err)
+		}
+
+		err = r.Push(&git.PushOptions{Auth: auth})
+        if err != nil {
+            log.Fatal(err)
+        }
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(syncCmd)
+
+	syncCmd.PersistentFlags().StringVar(&remoteUrl, "remote-url", "", "Remote repository URL")
+	syncCmd.PersistentFlags().StringVar(&keyFile, "key-file", "", "File containing the SSH key")
 }
--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/src-d/go-git v4.7.0+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
 	golang.org/x/net v0.0.0-20191014212845-da9a3fd4c582 // indirect
 	golang.org/x/sys v0.0.0-20191020152052-9984515f0562 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
