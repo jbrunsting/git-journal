c7abf4edc07bc8c00c6ffa6c632d0975a38496a4
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-12-29 17:11:40 -0500

    Make updates create seperate commits

--- git-journal.py
+++ git-journal.py
@@ -23,6 +23,35 @@ def files_exist():
     return os.path.exists(path_md) and os.path.exists(path_json)
 
 
+def commit_entry(entry_id, entry_timestamp, entry_body):
+    with open(path_json, "r+") as json_file:
+        entries = json.loads(json_file.read())
+        entries[entry_id] = {
+            "timestamp": entry_timestamp,
+            "body": entry_body
+        }
+        json_file.seek(0)
+        json_file.write(json.dumps(entries))
+        json_file.truncate()
+
+    with open(path_md, "w") as md_file:
+        sorted_entries = sorted(
+            entries.values(),
+            key=lambda e: time.strptime(e["timestamp"], TIME_FORMAT),
+            reverse=True)
+        formatted = "\n".join([
+            "## {}\n\n{}\n".format(entry["timestamp"], entry["body"])
+            for entry in sorted_entries
+        ])
+        md_file.write(formatted)
+
+    subprocess.run(["git", "add", JOURNAL_JSON])
+    subprocess.run(["git", "add", JOURNAL_MD])
+    title = entry_body.split("\n")[0]
+    subprocess.run(
+        ["git", "commit", "-m", title[:75] + (title[75:] and '...')])
+
+
 def update_from_origin():
     with open(path_json, "r+") as json_file:
         local_json = json.loads(json_file.read())
@@ -30,26 +59,13 @@ def update_from_origin():
     subprocess.run(["git", "fetch"])
     subprocess.run(["git", "reset", "--hard", "origin/master"])
     with open(path_json, "r+") as json_file:
-        combined_json = json.loads(json_file.read())
-        if combined_json == local_json:
+        origin_json = json.loads(json_file.read())
+        if origin_json == local_json:
             return False
 
-        combined_json.update(local_json)
-
-        json_file.seek(0)
-        json_file.write(json.dumps(combined_json))
-        json_file.truncate()
-
-    with open(path_md, "w") as md_file:
-        entries = sorted(
-            combined_json.values(),
-            key=lambda e: time.strptime(e["timestamp"], TIME_FORMAT),
-            reverse=True)
-        formatted = "\n".join([
-            "## {}\n\n{}\n".format(entry["timestamp"], entry["body"])
-            for entry in entries
-        ])
-        md_file.write(formatted)
+    new_entries = {k: v for k, v in local_json.items() if k not in origin_json}
+    for entry_id, entry in new_entries.items():
+        commit_entry(entry_id, entry["timestamp"], entry["body"])
 
     return True
 
@@ -63,7 +79,8 @@ def init():
 
     subprocess.run(["git", "init"])
     subprocess.run(["git", "remote", "add", "origin", remote])
-    subprocess.run(["git", "pull"])
+    subprocess.run(["git", "pull", "origin", "master"])
+    subprocess.run(["git", "push", "--set-upstream", "origin", "master"])
 
     if not files_exist():
         with open(path_json, "w") as json_file:
@@ -89,26 +106,9 @@ def add_entry():
         tf.flush()
         subprocess.run([editor, tf.name])
         tf.seek(0)
-
-        entry_id = str(uuid.uuid4())
-        entry_timestamp = time.strftime(TIME_FORMAT)
         entry_body = tf.read().decode("utf-8")
 
-    with open(path_json, "r+") as json_file:
-        local_json = json.loads(json_file.read())
-        local_json[entry_id] = {
-            "timestamp": entry_timestamp,
-            "body": entry_body
-        }
-        json_file.seek(0)
-        json_file.write(json.dumps(local_json))
-        json_file.truncate()
-
-    subprocess.run(["git", "add", JOURNAL_JSON])
-    subprocess.run(["git", "add", JOURNAL_MD])
-    title = entry_body.split("\n")[0]
-    subprocess.run(
-        ["git", "commit", "-m", title[:75] + (title[75:] and '...')])
+    commit_entry(str(uuid.uuid4()), time.strftime(TIME_FORMAT), entry_body)
 
 
 def sync():
@@ -121,9 +121,6 @@ def sync():
     subprocess.run(["git", "branch", "journal-backup"])
 
     if update_from_origin():
-        subprocess.run(["git", "add", JOURNAL_JSON])
-        subprocess.run(["git", "add", JOURNAL_MD])
-        subprocess.run(["git", "commit", "-m", "Sync remote journal"])
         subprocess.run(["git", "push"])
     else:
         print("Already up to date")
