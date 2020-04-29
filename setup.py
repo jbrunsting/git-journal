8daa4e71ba4d30d55191cdad82b52233f0c91a0f
Jacob Brunsting <jbrunsting@uwaterloo.ca>
2020-04-28 21:28:14 -0400

    Setup as package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+setup(
+    name='Git Journal',
+    version='1.0',
+    description='Journal backed by Git',
+    author='Jacob Brunsting',
+    author_email='jacob.brunsting@gmail.com',
+    python_requires='>=3',
+    scripts=['gjournal'],
+)
